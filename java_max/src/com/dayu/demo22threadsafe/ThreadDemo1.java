404efbe5a9181d227b29150359801b82e7291b87
dayu <dayubetter@gmail.com>
2026-04-15 23:40:16 +0800

    [java]study notes java max

---
+++ java_max/src/com/dayu/demo22threadsafe/ThreadDemo1.java
@@ -0,0 +1,16 @@
+package com.dayu.demo22threadsafe;
+
+public class ThreadDemo1 {
+    public static void main(String[] args) {
+        // 模拟线程安全问题
+        // 1. 设计一个账户类，用于创建小明小红的共同账户对象，存入10w元
+        Account acc = new Account("1001", 10000);
+        // 2. 设计线程类：创建小明和小红两个线程，模拟小明和小红同时去同一个账户取款10w。
+        new DrawThread("xm", acc).start();
+        new DrawThread("xh", acc).start();
+    }
+
+
+
+
+}
